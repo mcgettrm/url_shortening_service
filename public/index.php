761392ad95421895e08bef7b8e766dfc69ef540b
mcgettrm <mcgettrick.michael@gmail.com>
2021-07-01 20:43:38 +0100

    Working on registering application services

--- public/index.php
+++ public/index.php
@@ -6,6 +6,27 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
+
+//Register services
+$container = $app->getContainer();
+
+//UrlShortener Service
+$container[UrlShortenerService::class] = function($container){
+    return new UrlShortenerService();
+};
+
+//Url Validator
+$container[UrlValidator::class] = function($container){
+    return new UrlValidator();
+};
+
+//Repository
+$container[ShortLinkRepository::class] = function($container){
+    return new ShortLinkRepository();
+};
+
+
+//Register routes
 $app->get('/', function (Request $request, Response $response, array $args) {
     $response->getBody()->write("Hello world");
 
