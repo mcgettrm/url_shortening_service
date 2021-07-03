82ff2597b2bb04540f0c01eaebf5bcd5fd0352ec
mcgettrm <mcgettrick.michael@gmail.com>
2021-07-03 15:57:59 +0100

    Setting up the various test classes

---
+++ tests/DecodeTest.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class DecodeTest
+ *
+ * This class should cover the following scenarios
+ * - Should accept an IDENTIFIER.
+ * - Should validate that IDENTIFIER is 6 characters (or return status code 400).
+ * - Should validate that IDENTIFIER is made of valid HTTP URL characters (or return status code 400).
+ * - Should accept the IDENTIFIER with or without the CONFIG_URL
+ * - Should return the original LONG_URL on success (or return status code 404).
+ * - Should only accept SHORT_URL from the CONFIG_DOMAIN_NAME
+ * - If a short URL is passed that has an incorrect CONFIG_DOMAIN_NAME, fail and return status code 400
+ *
+ */
+class DecodeTest extends TestCase
+{
+
+    public function testTest(): void
+    {
+        $this->assertEquals(1,1);
+    }
+
+}
--- tests/EncodeTest.php
+++ tests/EncodeTest.php
@@ -1,11 +1,21 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class EncodeTest
+ *
+ * Should cover the following scenarios:
+ * - Should generate a 6-character IDENTIFIER from an input LONG_URL.
+ * - Should confirm that LONG_URL is a valid URL (or return status code 400).
+ * - Identifier should be unique.
+ * - Should return a string CONFIG_DOMAIN_NAME/IDENTIFIER to the caller.
+ * - IDENTIFIER should be a valid HTTP URL.
+ */
 class EncodeTest extends TestCase
 {
     public function testTest(): void
     {
-       $this->assertEquals(1,1);
+        $this->assertEquals(1,1);
     }
 
 }
