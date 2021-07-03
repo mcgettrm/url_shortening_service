13dd2165e5410e33bcf451eb904ecb2d9d5dd6b6
mcgettrm <mcgettrick.michael@gmail.com>
2021-07-03 21:36:47 +0100

    Working on decoding tests

--- tests/DecodeTest.php
+++ tests/DecodeTest.php
@@ -1,5 +1,6 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
+use UrlShortener\DomainObjects\Models\ShortLink;
 
 /**
  * Class DecodeTest
@@ -20,10 +21,20 @@ class DecodeTest extends AbstractUrlShortenerServiceTest
     private $genericDecodeString = "http://url_shortening_service/YTlkMT";
     public function testReturnsLongUrlIfIdentifierFoundInRepository(): void
     {
-        $config = $this->getConfig();
-        $shortenerService = $this->getGenericUrlShorteningService($config);
+        $longUrl = 'www.snuggle.com/marketing/my_amazing_campagin/extreme_pillows_deal';
+        $shortUrl = "http://url_shortening_service/YTlkMT";
+        $identifier = "YTlkMT";
 
-        $this->assertEquals(1,$shortenerService->decode($this->genericDecodeString));
+        $config = $this->getConfig();
+        $shortLinkRepository = $this->createMock(\UrlShortener\Repositories\ShortLinkRepository::class);
+        $shortLink = new ShortLink();
+        $shortLink->setLongUrl($longUrl);
+        $shortLink->setIdentifier($identifier);
+        $shortLink->setShortUrl($shortUrl);
+        $shortLinkRepository->method('read')->willReturn($shortLink);
+        $shortenerService = $this->getGenericUrlShorteningService($config, $shortLinkRepository);
+
+        $this->assertEquals($longUrl,$shortenerService->decode($this->genericDecodeString));
     }
 
 }
