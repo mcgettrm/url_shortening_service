3fe0de6b0f71df97fb3b89b219c6912003202ee5
mcgettrm <mcgettrick.michael@gmail.com>
2021-07-03 15:52:41 +0100

    First test

---
+++ tests/EncodeTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+class EncodeTest extends TestCase
+{
+    public function testTest(): void
+    {
+       $this->assertEquals(1,1);
+    }
+
+}
